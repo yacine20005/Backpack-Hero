e7f7bc4e54e0f456f57e0928ebbd62f2f0820c8e
Yacine <yacine200005@gmail.com>
2025-11-05 15:19:55 +0100

    add comments and formatting to Backpack and Position classes
    - Added explanatory comment to items field in Backpack.java to clarify use of Map interface for future implementation flexibility.
    - Added TODO comment in Position.java to note potential need for additional methods.
    - Improved code formatting with blank lines for better readability.

--- src/Backpack.java
+++ src/Backpack.java
@@ -2,9 +2,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Backpack {
+
     private final int width = 3;
     private final int height = 5;
-    private final Map<Position, Item> items;
+
+    private final Map<Position, Item> items; // Here we declare items as map and not as HashMap to increase the flexibility if we want to change the implementation later
 
     public Backpack() {
         this.items = new HashMap<>();
